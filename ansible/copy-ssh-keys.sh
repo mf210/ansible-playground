d0f34dd053db3e51fa7a5ac6f9b6c1a1a24df2ed
Mehdi Faresi <mfaresi210@gmail.com>
2023-10-21 16:13:52 +0330

    Update the copy-ssh-keys.sh script

--- ansible/copy-ssh-keys.sh
+++ ansible/copy-ssh-keys.sh
@@ -1,5 +1,7 @@
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
+
 for user in matt root
-do 
+do
   for server in server_1 server_2
   do
     sshpass -f password.txt ssh-copy-id -o StrictHostKeyChecking=no ${user}@${server}
